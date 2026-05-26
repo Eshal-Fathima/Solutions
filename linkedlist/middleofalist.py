7abc6b77eb32ab75eadcef5b6d58e279dfdecc30
Eshal Fathima <eshalf483@gmail.com>
2026-05-27 00:02:37 +0530

    middle of a list idea

--- linkedlist/middleofalist.py
+++ linkedlist/middleofalist.py
@@ -3,3 +3,16 @@
 #fast == move 2 spaces
 #slow + 1 == fast + 2
 
+class Node:
+    def __init__(self, val = 0, next = none):
+        self.val = val
+        self.next = next
+class Solution:
+    def middle(self,head):
+        slow, fast = head,head
+        while fast and fast.next:
+            slow = slow.next 
+            fast = fast.next.next
+        return slow
+
+#uses 2 pointer technique
