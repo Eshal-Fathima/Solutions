a0103688bdd8e1203b23b512fce7503accd7d363
Eshal Fathima <eshalf483@gmail.com>
2026-05-29 23:54:49 +0530

    remove duplicates for sorted list

---
+++ linkedlist/removeduplicatesinsorted.py
@@ -0,0 +1,4 @@
+# here you use the current pointer
+# if currentnext == current.next.next
+#then it will automatically remove the list in the middle by moving to the 
+#end node skipping the duplicate 
