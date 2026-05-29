2e9bd3aa025159223dc3bc5a16402f5be57314f9
Eshal Fathima <eshalf483@gmail.com>
2026-05-29 23:51:04 +0530

    daily leetcode problem

--- dailychallenges/3300.py
+++ dailychallenges/3300.py
@@ -4,3 +4,15 @@
 # and find the minimum of that number
 # eg : 10 == 1+0 = 1
  
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        ans = float('inf')            #gives infinity value so that it is not = 0 meaning
+        #number given will be always greater than the given current number == infinity
+        for i in nums:
+            res = 0
+            while i:        #while i is not equal to 0 is the condition here 
+                res += i % 10
+                i = i // 10
+            ans = min(res, ans)     #compares the res of the digits, and answer gives you 
+            # the minimum value among them
+        return ans
