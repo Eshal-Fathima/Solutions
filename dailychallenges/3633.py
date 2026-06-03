c801bb212da0bfb5ce6d64b55d972649bd2bc1ca
Eshal Fathima <eshalf483@gmail.com>
2026-06-03 23:52:30 +0530

    daily leetcode problem

--- dailychallenges/3633.py
+++ dailychallenges/3633.py
@@ -1,3 +1,4 @@
+#earliest finish time for land and water
 class Solution:
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
         res = []
