a104e722dc2a4534936d77a35b0b49ee6f91ac99
Eshal Fathima <eshalf483@gmail.com>
2026-06-09 23:24:42 +0530

    daily leetcode problem

--- maxsubarray/totalsaforvalue1.py
+++ maxsubarray/totalsaforvalue1.py
@@ -8,9 +8,14 @@ class Solution:
         return count 
     
 # brute force method or direct computation method
+# time complexity = O(n+k)
+# space complexity = O(1)
 
 # more optimized version 
 class Solution:
     def maxTotalValue(self, nums: List[int], k: int) -> int:
         return (max(nums)-min(nums))*k
     
+# time complexity = O(n)
+# space complexity = O(1)
+# approach: Brute force or direct approach
