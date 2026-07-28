187ef095e057a4557cf71cb3ed45fe47956b30a2
Eshal Fathima <eshalf483@gmail.com>
2026-07-28 22:56:10 +0530

    maxsubarray

--- maxsubarray/totalsaforvalue1.py
+++ maxsubarray/totalsaforvalue1.py
@@ -18,4 +18,5 @@ class Solution:
     
 # time complexity = O(n)
 # space complexity = O(1)
+#maxsubarray uses kadens algorithm to find the maximum sum of a contiguous subarray. The approach is to iterate through the array and keep track of the current sum and the maximum sum found so far. If the current sum becomes negative, reset it to zero. The maximum sum will be the answer at the end of the iteration.
 # approach: Brute force or direct approach
